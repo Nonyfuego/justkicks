bc90256531b597e5e15344c67cd045b72fa328ce
nonyfuego <nonykuti@gmail.com>
2023-02-26 00:11:48 +0100

    make changes to the secret key

--- src/justkicks/settings.py
+++ src/justkicks/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-an4ovbk4w0*6w0_r%j$p8znus=jlou7y4cn%&xw@02e)cll$&_'
+SECRET_KEY = os.getenv("JUSTKICKS_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
